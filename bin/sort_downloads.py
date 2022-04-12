9c4519e2cccd354047303c8c3bc3ce86c55acb2d
Vikram Venkataranan <14256527+viktree@users.noreply.github.com>
2022-04-11 23:41:05 -0400

    use built in join

--- bin/sort_downloads.py
+++ bin/sort_downloads.py
@@ -1,6 +1,5 @@
 #!python3
 
-from email.generator import Generator
 from os import listdir, environ
 from os.path import isfile, join
 import os
@@ -14,14 +13,17 @@ ignore_list = [".DS_Store"]
 def is_sortable(f: str) -> bool:
     if not isfile(join(DOWNLOADS_PATH, f)):
         return False
+
     if f in ignore_list:
         return False
+
     filename_split = f.split(".")
     return len(filename_split) > 1
 
 
-def get_filetype(full_filename):
+def get_filetype(full_filename: str) -> str:
     extension = full_filename.split(".")[-1]
+
     compressed = ["gz", "tar", "zip"]
     if extension in compressed:
         return "compressed"
@@ -29,6 +31,7 @@ def get_filetype(full_filename):
     games = ["gb", "nds"]
     if extension in games:
         return "games"
+
     return extension
 
 
@@ -43,8 +46,8 @@ def put_files_in_folders():
 
         ftype = get_filetype(full_filename)
 
-        src_path = DOWNLOADS_PATH + "/" + full_filename
-        dest_path = DOWNLOADS_PATH + "/" + ftype
+        src_path = join(DOWNLOADS_PATH, full_filename)
+        dest_path = join(DOWNLOADS_PATH, ftype)
 
         ftype_folder_paths.add(dest_path)
         files_to_sort.append((src_path, dest_path))
@@ -55,10 +58,11 @@ def put_files_in_folders():
             os.mkdir(ftype_folder_path)
 
     # Move files
-    for i, file_to_sort in enumerate(files_to_sort):
-        src_path, dest_path = file_to_sort
+    i = 1
+    for src_path, dest_path in files_to_sort:
         shutil.move(src_path, dest_path)
-        print(str(i + 1) + " :" + src_path + ">>>" + dest_path)
+        print(str(i) + " :" + src_path + ">>>" + dest_path)
+        i += 1
 
 
 if __name__ == "__main__":
