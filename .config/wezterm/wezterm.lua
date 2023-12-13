72a2aacd4dd528c38e3303be0c49f37b96a3f081
Vikram Venkataramanan <vikram.v@ecobee.com>
2023-12-13 11:01:12 -0500

    formatting

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require "wezterm"
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,20 +7,21 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
 -- config.color_scheme = 'Catppuccin Mocha'
-config.color_scheme = 'Atom'
+config.color_scheme = "OneHalfDark"
 
 -- config.font = wezterm.font 'Iosevka Nerd Font'
 
-config.font = wezterm.font_with_fallback {
-  'Iosevka Nerd Font Mono',
-  'Monaspace Neon',
+config.font =
+    wezterm.font_with_fallback {
+    "Iosevka Nerd Font Mono",
+    "Monaspace Neon"
 }
 
 config.font_size = 14
